1323f88c9e8e2a1d1da34d3897cfc265dca76cd4
Przemysław <puzon_p@o2.pl>
2017-11-27 13:25:42 +0100

    update

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-$('a').click(function(){
+$(document).ready(function() {
+  $('a').click(function(){
     $('html, body').animate({
         scrollTop: $( $(this).attr('href') ).offset().top
     }, 500);
     return false;
+  });
+  $('#mobile').click(function(){
+        if($('#dropDown').is(':visible')) {
+        $('#dropDown').slideUp();
+        } else {
+        $('#dropDown').slideDown();    
+        }
+
+    });
 });
